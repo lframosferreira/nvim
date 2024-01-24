b40a7d6352cf226b6765dc377a15d7b06546d81e
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-01-24 15:43:28 -0300

    feat: EP 4: adding linters and formatters with none ls

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,29 @@
+return {
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                -- Lua
+                null_ls.builtins.formatting.stylua,
+
+                -- JS/TS
+                null_ls.builtins.formatting.eslint,
+                null_ls.builtins.diagnostics.eslint_d,
+
+                -- C/C++
+                null_ls.builtins.formatting.clang_format,
+
+                -- Rust
+                null_ls.builtins.formatting.ruff,
+
+                -- Python
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.diagnostics.mypy,
+                null_ls.builtins.diagnostics.flake8,
+            },
+        })
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+}
